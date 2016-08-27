b7599b13ae3f9287cbb576faa420f8e51df8214b
charlie_weng <charliexp.weng@gmail.com>
2016-08-27 10:39:19 +0800

    fix some bug
    Signed-off-by: charlie_weng <charliexp.weng@gmail.com>

--- examples/client.js
+++ examples/client.js
@@ -1,12 +1,40 @@
-'use strict';
-var mqtt = require('mqtt');
+/*************************************************************************\
+ * File Name    : client.js                                              *
+ * --------------------------------------------------------------------- *
+ * Title        :                                                        *
+ * Revision     : V1.0                                                   *
+ * Notes        :                                                        *
+ * --------------------------------------------------------------------- *
+ * Revision History:                                                     *
+ *   When             Who         Revision       Description of change   *
+ * -----------    -----------    ---------      ------------------------ *
+ * 12-23-2015      charlie_weng     V1.0          Created the program     *
+ *                                                                       *
+\*************************************************************************/
+var mqtt    = require('mqtt');
 var mqttrpc = require('../index.js');
-var debug = require('debug')('remote-time:client');
 
-var mqttclient = mqtt.createClient();
+var settings = {
+  reconnectPeriod: 5000 // chill on the reconnects
+}
 
+// client connection
+//var mqttclient = mqtt.connect('mqtt://localhost', settings);
+var mqttclient = mqtt.connect('mqtt://test1:test1@127.0.0.1:1883', settings);
+
+// build a new RPC client
 var client = mqttrpc.client(mqttclient);
 
-client.callRemote('$RPC/time', 'localtime', {}, function(err, data){
-  debug('callRemote', err, data);
+// optionally configure the codec, which defaults to JSON, also supports msgpack
+//client.format('json');
+client.format('msgpack');
+
+// call the remote method
+setInterval(function(){
+    client.callRemote('proto/time', 'localtime',{}, function(err, data){
+        console.log('callRemote',err,data);
+    }); 
+},5000);
+client.callRemote('proto/time', 'localtime',{}, function(err, data){
+   console.log('callRemote',err, data );
 });
--- examples/server.js
+++ examples/server.js
@@ -1,12 +1,37 @@
-'use strict';
+/*************************************************************************\
+ * File Name    : client.js                                              *
+ * --------------------------------------------------------------------- *
+ * Title        :                                                        *
+ * Revision     : V1.0                                                   *
+ * Notes        :                                                        *
+ * --------------------------------------------------------------------- *
+ * Revision History:                                                     *
+ *   When             Who         Revision       Description of change   *
+ * -----------    -----------    ---------      ------------------------ *
+ * 12-23-2015      charlie_weng     V1.0          Created the program     *
+ *                                                                       *
+\*************************************************************************/
 var mqtt = require('mqtt');
 var mqttrpc = require('../index.js');
-var debug = require('debug')('remote-time:server');
 
-var client = mqtt.createClient();
-var server = mqttrpc.server(client);
+var settings = {
+  reconnectPeriod: 5000 // chill on the reconnects
+}
 
-server.provide('$RPC/time', 'localtime', function (args, cb) {
-  debug('localtime');
+// client connection
+//var mqttclient = mqtt.connect('mqtt://localhost', settings);
+var mqttclient = mqtt.connect('mqtt://test1:test1@127.0.0.1:1883', settings);
+
+// build a mqtt new RPC server
+var server = mqttrpc.server(mqttclient);
+
+// optionally configure the codec, which defaults to JSON, also supports msgpack
+//server.format('json');
+server.format('msgpack');
+
+// provide a new method
+server.provide('proto/time', 'localtime', function (args, cb) {
+  console.log('localtime',new Date());
   cb(null, new Date());
 });
+
